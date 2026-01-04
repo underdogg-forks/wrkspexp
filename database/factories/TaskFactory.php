c81a29abb2e11dadd5c329a7bcc07284e407b2f4
Niels Drost <47660417+nielsdrost7@users.noreply.github.com>
2026-01-04 16:03:12 +0100

    Fix title assignment in TaskFactory

--- database/factories/TaskFactory.php
+++ database/factories/TaskFactory.php
@@ -18,8 +18,8 @@ class TaskFactory extends Factory
             'client_id' => Client::factory(),
             'project_id' => fake()->optional()->randomElement([null, Project::factory()]),
             'task_number' => 'TSK-' . fake()->unique()->numberBetween(10000, 99999),
-            'title' => fake()->sentence(),
             'status' => fake()->randomElement(TaskStatus::cases())->value,
+            'title' => fake()->sentence(),
             'estimated_hours' => fake()->optional()->numberBetween(1, 100),
             'due_at' => fake()->optional()->dateTimeBetween('now', '+90 days'),
         ];

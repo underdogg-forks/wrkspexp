b860667f199a31fb6f088f1c5e1e0b9c543e90a4
Niels Drost <47660417+nielsdrost7@users.noreply.github.com>
2026-01-04 16:08:33 +0100

    Add slug, tax_id, and tax_number columns to relations table

--- database/migrations/2026_01_04_120500_create_relations_table.php
+++ database/migrations/2026_01_04_120500_create_relations_table.php
@@ -12,6 +12,9 @@ return new class extends Migration
             $table->id();
             $table->string('relation_number')->unique();
             $table->string('name');
+            $table->string('slug');
+            $table->string('tax_id');
+            $table->string('tax_number');
         });
     }
 
